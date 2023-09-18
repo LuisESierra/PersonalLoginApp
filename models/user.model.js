a34c579bef6923335dd52fe92ea9777c3ab7b8c1
LuisESierra <luis_enrique.sierra@uao.edu.co>
2023-09-18 00:26:27 -0500

    No tengo ni idea

--- models/user.model.js
+++ models/user.model.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    identification: {
         type: String,
         required: true,
         trim: true
     },
     email: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
         type: String,
         required: true,
         trim: true
